6f1b463259f51b723e7ea7b2795b453ca3bd2466
kirinrastogi <rastogikirin@gmail.com>
2016-12-19 20:20:18 +0000

    initial commit

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+# Varibles
+backup=~/dotfiles_backup
+dir=$(pwd)
+dotfiles=".vimrc .vim .bashrc .fehbg .fonts .gitconfig .gtkrc-2.0 .i3 .logout .nvidia-settings-rc .Xauthority .xorg .zshrc"
+
+# Backup
+mkdir -p $backup
+echo "Backup dir: $backup"
+
+# Copy files, and create sym links to current dir (~/dotfiles)
+for dotfile in $dotfiles; do
+	mv ~/$dotfile $backup
+	echo "Current $dotfile moved to: $backup"
+	ln -s $dir/$dotfile ~/$dotfile
+	echo "Symbloic link for $dotfile made in ~"
+done
+
+# Dir for backup/swap files
+mkdir -p ~/.vim/.backup
+mkdir -p ~/.vim/.swap
+mkdir -p ~/.vim/.undo
+echo "Dirs made for backup/swap/undo files"
